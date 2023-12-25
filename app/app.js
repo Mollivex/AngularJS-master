038486e421d671041eecb4c528662f0831a48bfc
Alexandr Cocier <licoms1997@gmail.com>
2023-12-25 15:39:32 +0200

    Controllers section - scope, dependencies etc.

--- app/app.js
+++ app/app.js
@@ -1,14 +1,9 @@
 var myNinjaApp = angular.module('myNinjaAppName', [])
 
-myNinjaApp.config(function(){
+myNinjaApp.controller('NinjaController', ['$scope', 'dependency',function($scope,$dependency){
 
+    $scope.message = "hey y'all";
 
-});
+    $scope.ninjas = ['yoshi', 'cristal', 'ryu', 'shaun', 'sukiro', 'karioshi'];
 
-myNinjaApp.run(function(){
-
-
-});
-
-myNinjaApp.controller
-
+}]);
