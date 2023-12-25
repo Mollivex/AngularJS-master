ddcd45a1a047a569335aaabfbe0a33838b7084dd
Alexandr Cocier <licoms1997@gmail.com>
2023-12-25 16:30:52 +0200

    ng-show (ng-hide) directive

--- app/app.js
+++ app/app.js
@@ -8,33 +8,38 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
         {
             name: "Yoshi",
             belt: "green",
-            rate: 35
+            rate: 35,
+            available: false
         },
         {
             name: "Cristal",
             belt: "red",
-            rate: 400
+            rate: 400,
+            available: true
         },
         {
             name: "Ryu",
             belt: "black",
-            rate: 500
-            
+            rate: 500,
+            available: true
         },
         {
             name: "Shaun",
             belt: "blue",
-            rate: 25
+            rate: 25,
+            available: true
         },
         {
             name: "Sukiro",
             belt: "yellow",
-            rate: 15
+            rate: 15,
+            available: false
         },
         {
             name: "Karioshi",
             belt: "white",
-            rate: 50
+            rate: 50,
+            available: true
         }
     ];
 
