3d5b3bb8ca9e8d4621ca4334280ef00d601d5794
Alexandr Cocier <licoms1997@gmail.com>
2023-12-25 16:04:03 +0200

    Filters section - adding filters and rates

--- app/app.js
+++ app/app.js
@@ -1,9 +1,41 @@
 var myNinjaApp = angular.module('myNinjaAppName', [])
 
-myNinjaApp.controller('NinjaController', ['$scope', 'dependency',function($scope,$dependency){
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 
     $scope.message = "hey y'all";
 
-    $scope.ninjas = ['yoshi', 'cristal', 'ryu', 'shaun', 'sukiro', 'karioshi'];
+    $scope.ninjas = [
+        {
+            name: "Yoshi",
+            belt: "green",
+            rate: 35
+        },
+        {
+            name: "Cristal",
+            belt: "red",
+            rate: 400
+        },
+        {
+            name: "Ryu",
+            belt: "black",
+            rate: 500
+            
+        },
+        {
+            name: "Shaun",
+            belt: "blue",
+            rate: 25
+        },
+        {
+            name: "Sukiro",
+            belt: "yellow",
+            rate: 15
+        },
+        {
+            name: "Karioshi",
+            belt: "white",
+            rate: 50
+        }
+    ];
 
 }]);
